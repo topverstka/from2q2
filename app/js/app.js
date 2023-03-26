64f20b7ab3624696f4b31a511170456a71b709b1
BagFreman <mr.bagfreman@gmail.com>
2023-03-26 21:38:12 +0300

    update

--- app/js/app.js
+++ app/js/app.js
@@ -10,5 +10,67 @@ document.addEventListener('DOMContentLoaded', () => {
           document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
      })
 
+     gsap.to(".front-el__2", {
+          scale: 1,
+          y: 100,
+          opacity: 1,
+          duration: 50,
+          transition: "linear",
+          scrollTrigger: {
+               trigger: ".front-el__2",
+               start: "top 70%",
+               end: "bottom 20%",
+               scrub: 0,
+          }
+
+     })
+
+     gsap.to(".front-el__1", {
+          scale: 1,
+          y: 100,
+          opacity: 1,
+          duration: 50,
+          transition: "linear",
+          scrollTrigger: {
+               trigger: ".section__main-article",
+               start: "top 70%",
+               end: "bottom 20%",
+               scrub: 0,
+          }
+
+     })
+
+     gsap.to(".main-news-letter__el-4", {
+          scale: 1,
+          y: 50,
+          opacity: 1,
+          duration: 50,
+          transition: "linear",
+          scrollTrigger: {
+               trigger: ".section__main-news-letter",
+               start: "top 70%",
+               end: "bottom 20%",
+               scrub: 0,
+               markers: true,
+          }
+
+     })
+
+     gsap.to(".footer__el-1, .footer__el-2", {
+          scale: 1,
+          y: 100,
+          opacity: 1,
+          duration: 50,
+          transition: "linear",
+          scrollTrigger: {
+               trigger: ".footer",
+               start: "top 70%",
+               end: "bottom 20%",
+               scrub: 0,
+               markers: true,
+          }
+
+     })
+
 
 })
